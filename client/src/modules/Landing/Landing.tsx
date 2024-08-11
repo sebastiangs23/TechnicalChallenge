c8a584aabdd0074b64641846a502b72eb4e0e51e
sebastiangs23 <sebastiangs2309@gmail.com>
2024-08-11 15:16:06 -0500

    feat(Client): :sparkles: Creating the project

---
+++ client/src/modules/Landing/Landing.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+
+const Landing: React.FC = () => {
+    return (
+        <div>
+            <h2>Landing</h2>
+            <SignIn />
+            <SignUp />
+        </div>
+    )
+};
+
+export default Landing;
