b20a5af935b3617733fe8011f83195a77ed1606b
sebastiangs23 <sebastiangs2309@gmail.com>
2024-08-12 01:00:11 -0500

    feat(Api): :sparkles: eY

--- api/src/controllers/users/usersController.ts
+++ api/src/controllers/users/usersController.ts
@@ -6,7 +6,7 @@ export async function getUsers(req: Request, res: Response) {
   try {
     const users = await User.find({});
 
-    console.log("los usuarios :3", users);
+    console.log("los usuarios: ", users);
 
     res.json(users);
   } catch (error) {
