b3af6e773a7245d33575e10900ee13de23105706
sebastiangs23 <sebastiangs2309@gmail.com>
2024-08-14 03:05:06 -0500

    docs(Api && Client): :memo: Doc Swagger

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,15 +1,29 @@
 import { Router } from "express";
 import userRoutes from "./users/index.js";
 import assisntanceRoutes from "./assinstants/index.js";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Obtener la ruta del archivo actual
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const swaggerDocument = YAML.load(path.join(__dirname, "../../assistant_doc_swagger.yaml"));
 
 const router = Router();
 
 /*__________
 |  USERS  */
-router.use('/users', userRoutes);   
+router.use('/users', userRoutes);
 
 /*_____________
 |  CHAT-GPT  */
 router.use('/assistants', assisntanceRoutes);
+
+/*_________________
+|  DOCUMENTATION  */
+router.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default router;
